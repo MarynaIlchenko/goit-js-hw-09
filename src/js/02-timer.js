b14adf5cc515081a7c372bcb2f1bf3b11a3fe8e5
MarynaIlchenko <debovamail@gmail.com>
2022-02-04 15:46:55 +0200

    last update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const refs = {
   datetimePicker: document.querySelector('input#datetime-picker'),
@@ -9,20 +10,42 @@ const refs = {
   minutes: document.querySelector('span[data-minutes]'),
   seconds: document.querySelector('span[data-seconds]'),
 };
+    let time = null;
+refs.startBtn.disabled = false;
 
-const timer {
-    start() {
-        const startTime = Date.now();
 
-        setInterval(() => {
-            const currentTime = Date.now();
-            const deltaTime = currentTime - startTime;
-            const timeComponents = convertMs(deltaTime);
-        }, 1000);
-    },
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+ onClose(selectedDates) {
+      const selectedDate = selectedDates[0];
+     const startTime = Date.now();
+       if (selectedDate < startTime) {
+      Notify.failure('Please choose future date in the calendar');
+      refs.startButton.disabled = true;
+      return;
+    // console.log(selectedDates[0]);
+  },
 };
 
-timer.start();
+refs.startBtn.addEventListener('click', startCountdown);
+
+ function startCountdown() {
+      refs.startBtn.disabled = true;
+     refs.datetimePicker.disabled = true;
+     
+time = setInterval(() => {
+            const currentTime = Date.now();
+            const deltaTime = currentTime - startTime;
+            const { days, hours, minutes, seconds } = convertMs(deltaTime);
+        }, 1000);
+    }
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -32,20 +55,17 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
 
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
 // enableTime - Включает устройство выбора времени (default - false)
 //time_24hr - Отображает устройство выбора времени в 24-часовом режиме без выбора AM/PM, если включено.
